94f621f87c3b02abce6e6d2d1a24d5c967df1125
vuust <49905277+vuust@users.noreply.github.com>
2019-08-21 11:08:42 +0200

    maybe again

--- Content/book.js
+++ Content/book.js
@@ -9,12 +9,14 @@ button.onclick = function() {
         firstname.focus();
         return false;
     }
-    //tjek input:text felt email
-    const email = document.getElementById('email');
-    if(email.value === '') {
-        alert('Feltet email må ikke være tomt!')
-        email.focus();
-        return false;
+
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
     }
 
     //tjek input:text felt nummer
--- Content/medlem.js
+++ Content/medlem.js
@@ -9,13 +9,15 @@ if(firstname.value === '') {
     firstname.focus();
     return false;
 }
-//tjek input:text felt email
-const email = document.getElementById('email');
-if(email.value === '') {
-    alert('Feltet email må ikke være tomt!')
-    email.focus();
+
+var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
     return false;
-}
+    }
 
 //tjek input:text felt nummer
 const number = document.getElementById('number');
